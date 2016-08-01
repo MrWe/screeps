cf71097c5961b5044b8290078beed4301767d6f2
mrwe <erikwik@stud.ntnu.no>
2016-08-01 19:49:01 +0200

    Error

--- phaseOne.js
+++ phaseOne.js
@@ -15,20 +15,21 @@ var phaseOne = {
 		var creepSize = [WORK,CARRY,CARRY,MOVE,MOVE];
 		cleanMemoryCreep.run;
 		spawn(creepSize);
-	},
-	spawn: function(creepSize){
-		var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-
-        if(harvesters <= 5){
-        	var newName = Game.spawns['Spawn'].createCreep(creepSize, undefined, {role: 'harvester'});
-            console.log('Spawning new harvester: ' + newName);
-        }
-        else if(upgraders <= 5){
-        	var newName = Game.spawns['Spawn'].createCreep(creepSize, undefined, {role: 'upgrader'});
-            console.log('Spawning new upgrader: ' + newName);
-        }
 	}
 };
+
+var spawn = function(creepSize){
+	var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+
+    if(harvesters <= 5){
+    	var newName = Game.spawns['Spawn'].createCreep(creepSize, undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    else if(upgraders <= 5){
+    	var newName = Game.spawns['Spawn'].createCreep(creepSize, undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader: ' + newName);
+    }
+}
 
 module.exports = phaseOne;
